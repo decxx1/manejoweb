c8aa390cced8827a96a69cff7b8ac96872ca3071
Damian Cisternas <decxx1@gmail.com>
2024-01-21 18:42:16 -0300

    fix details

--- src/components/BotonesContacto.vue
+++ src/components/BotonesContacto.vue
@@ -64,7 +64,6 @@
     },
     computed: {
       isMobile() {
-        this.$nextTick
         const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
         return regex.test(navigator.userAgent);
       }
